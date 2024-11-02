7b1107c40e3242a389a8500e01e202852f858555
selfishAltruism <officialkyus@gmail.com>
2024-11-02 18:28:52 +0900

    feat: implement button

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -2,3 +2,5 @@ export { Background } from "./screen/Background";
 export { GrayBackground } from "./screen/GrayBackground";
 export { Loading } from "./screen/Loading";
 export { Header } from "./layout/Header";
+
+export { BlueButton, OrangeButton, OrangeTwoButton } from "./element/Button";
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,20 @@
 import styled from "@emotion/styled";
 
+import { BlueButton, OrangeTwoButton } from "@/entities";
+
 const HomePage = () => {
-  return <>Home</>;
+  return (
+    <>
+      Home
+      <BlueButton>Blue</BlueButton>
+      <OrangeTwoButton
+        rightHandler={() => {}}
+        leftHandler={() => {}}
+        leftText="Left"
+        rightText="Right"
+      />
+    </>
+  );
 };
 
 export default HomePage;
