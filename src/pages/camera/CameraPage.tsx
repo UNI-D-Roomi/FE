acfa7d4a5d193a16c8035f583cdde31dc0a1143b
selfishAltruism <officialkyus@gmail.com>
2024-11-03 09:54:24 +0900

    fix: v1

--- src/pages/camera/CameraPage.tsx
+++ src/pages/camera/CameraPage.tsx
@@ -14,51 +14,98 @@ const CameraPage = () => {
 
   const { upload, startDish, endDish, endRoom, setImg } = UserService();
 
-  const camera = useRef<{
-    width: string;
-    height: string;
-    takePhoto: () => React.SetStateAction<null>;
-    switchCamera: () => void;
-  }>(null);
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [capturedImage, setCapturedImage] = useState(null);
 
   useEffect(() => {
-    camera.current.switchCamera();
+    const startCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: { facingMode: { exact: "environment" } },
+        });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error("Error accessing camera: ", error);
+      }
+    };
+
+    startCamera();
+
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        let stream = videoRef.current.srcObject;
+        let tracks = stream.getTracks();
+        tracks.forEach((track) => track.stop());
+      }
+    };
   }, []);
 
-  const [image, setImage] = useState(null);
+  const capturePhoto = () => {
+    if (canvasRef.current && videoRef.current) {
+      const canvas = canvasRef.current;
+      const video = videoRef.current;
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const context = canvas.getContext("2d");
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  useEffect(() => {
-    if (image) {
-      const formData = new FormData();
-      formData.append("file", dataURLtoFile(image, "file"));
-      upload(formData).then((res) => {
-        if (location.state.mode === "BEFOREDISH")
-          startDish(res).then(() => {
-            navigate("/dish", { state: { stage: 2, score: 0, comment: "" } });
-          });
-        else if (location.state.mode === "AFTERDISH")
-          endDish(res).then((res) => {
-            navigate("/dish", {
-              state: { stage: 3, score: res.score, comment: res.comment },
-            });
-          });
-        else if (location.state.mode === "ROOM")
-          endRoom(res).then((res) => {
-            navigate("/room", {
-              state: { stage: 1, score: res.score, comment: res.comment },
-            });
-          });
-        else
-          setImg(res).then(() => {
-            navigate(PAGE_URL.SignIn);
-          });
-      });
+      // 캡처한 이미지를 데이터 URL로 변환하여 상태에 저장
+      const imageDataUrl = canvas.toDataURL("image/png");
+      setCapturedImage(imageDataUrl);
+
+      // 데이터 URL을 파일로 변환하고 서버에 전송
+      const file = dataURLtoFile(imageDataUrl, "captured-image.png");
+      uploadToServer(file);
     }
-  }, [image]);
+  };
+
+  // 데이터 URL을 파일로 변환하는 함수
+  const dataURLtoFile = (dataUrl, filename) => {
+    const arr = dataUrl.split(",");
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+  };
+
+  // 서버에 파일 업로드하는 함수
+  const uploadToServer = async (file) => {
+    const formData = new FormData();
+    formData.append("file", dataURLtoFile(file, "file"));
+    upload(formData).then((res) => {
+      if (location.state.mode === "BEFOREDISH")
+        startDish(res).then(() => {
+          navigate("/dish", { state: { stage: 2, score: 0, comment: "" } });
+        });
+      else if (location.state.mode === "AFTERDISH")
+        endDish(res).then((res) => {
+          navigate("/dish", {
+            state: { stage: 3, score: res.score, comment: res.comment },
+          });
+        });
+      else if (location.state.mode === "ROOM")
+        endRoom(res).then((res) => {
+          navigate("/room", {
+            state: { stage: 1, score: res.score, comment: res.comment },
+          });
+        });
+      else
+        setImg(res).then(() => {
+          navigate(PAGE_URL.SignIn);
+        });
+    });
+  };
 
   return (
     <>
-      {image ? <Loading /> : null}
+      {capturedImage ? <Loading /> : null}
       <Target1>
         <img src="/icons/target1.svg" alt="target" />
       </Target1>
@@ -72,23 +119,8 @@ const CameraPage = () => {
         <img src="/icons/target4.svg" alt="target" />
       </Target4>
       <CameraContainer>
-        <Camera
-          ref={camera}
-          errorMessages={{
-            noCameraAccessible:
-              "No camera device accessible. Please connect your camera or try a different browser.",
-            permissionDenied:
-              "Permission denied. Please refresh and give camera permission.",
-            switchCamera:
-              "It is not possible to switch camera to different one because there is only one video device accessible.",
-            canvas: "Canvas is not supported.",
-          }}
-        />
-        <SubmitButton
-          onClick={() => {
-            setImage(camera.current!.takePhoto());
-          }}
-        >
+        <video ref={videoRef} autoPlay playsInline style={{ width: "100%" }} />
+        <SubmitButton onClick={capturePhoto}>
           <CameraIcon fontSize="large" />
         </SubmitButton>
       </CameraContainer>
@@ -98,7 +130,7 @@ const CameraPage = () => {
 
 const CameraContainer = styled.div`
   height: 100%;
-  div {
+  video {
     position: absolute;
     top: 0px;
     width: 100%;
